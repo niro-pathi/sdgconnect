ededa2f2dc8ffa0947a8387faaa824e894c7bef6
Niro <niroshana.pathirana@crownmelbourne.com.au>
2016-02-12 08:51:27 +1100

    added spalshscreen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('sdgconnect.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaSplashscreen) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,8 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+   $cordovaSplashscreen.show();
 
   // Form data for the login modal
   $scope.loginData = {};
